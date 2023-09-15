2a5ef38f3d3f2b10a609516b45a653b0e39eda20
Leonardo Lopes <leonardolopesborges@hotmail.com>
2023-09-15 09:28:10 -0300

    update

--- App.js
+++ App.js
@@ -4,9 +4,11 @@ AFRAME.registerComponent("markerhandler", {
         const markerCachorro = document.getElementById("marker-cachorro");
         const markerCrocodilo = document.getElementById("marker-crocodilo");
         const markerCobra = document.getElementById("marker-cobra");
+        const markerCat = document.getElementById("marker-cat");
         const botaoCachorro = document.getElementById("botao-cachorro");
         const botaoCrocodilo = document.getElementById("botao-crocodilo");
         const botaoCobra = document.getElementById("botao-cobra");
+        const botaoCat = document.getElementById("botao-cat");
 
         markerCachorro.addEventListener("markerFound", function () {
             // Marcador do Cachorro detectado, exibir o botão do Cachorro
@@ -45,6 +47,19 @@ AFRAME.registerComponent("markerhandler", {
             botaoCobra.style.display = "none";
         });
 
+        markerCat.addEventListener("markerFound", function () {
+            // Marcador do Cobra detectado, exibir o botão do Cobra
+            botaoCat.style.display = "block"; // Ocultar o botão do Cachorro
+            botaoCobra.style.display = "none";
+            botaoCrocodilo.style.display = "none"; // Ocultar o botão do Crocodilo
+            botaoCachorro.style.display = "none"; // Ocultar o botão do Cachorro
+        });
+
+        markerCobra.addEventListener("markerLost", function () {
+            // Marcador do Cachorro perdido, ocultar o botão do Cachorro
+            botaoCat.style.display = "none";
+        });
+
         // Evento de clique no botão do Cachorro
         botaoCachorro.addEventListener("click", function () {
             alert("Cachorro Clicado!");
@@ -67,6 +82,15 @@ AFRAME.registerComponent("markerhandler", {
          botaoCobra.addEventListener("click", function () {
             alert("Cobra Clicada!");
             const soundEl = document.getElementById("snake-sound");
+            if (soundEl) {
+                soundEl.play();
+            }
+        });
+
+        // Evento de clique no botão do Gato
+        botaoCat.addEventListener("click", function () {
+            alert("Gato Clicado!");
+            const soundEl = document.getElementById("cat-sound");
             if (soundEl) {
                 soundEl.play();
             }
