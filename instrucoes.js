6a32a383c455c2b5f2f459015dc0bb166aa2e6dc
Leonardo Lopes <leonardolopesborges@hotmail.com>
2023-11-30 10:54:11 -0300

    update

--- instrucoes.js
+++ instrucoes.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Variável para armazenar o elemento de áudio atualmente em reprodução
+    let currentlyPlaying;
+
     // Selecione todos os botões com a classe "animal-button"
     const animalButtons = document.querySelectorAll(".animal-button");
 
@@ -11,10 +14,17 @@ document.addEventListener("DOMContentLoaded", function () {
             // Obtenha o elemento de áudio correto com base no ID
             const soundEl = document.getElementById(soundId);
 
+            // Pausa a reprodução se houver algum som em reprodução
+            if (currentlyPlaying) {
+                currentlyPlaying.pause();
+            }
+
             // Verifique se o elemento de áudio existe
             if (soundEl) {
                 // Reproduza o som associado ao animal
                 soundEl.play();
+                // Atualize o elemento de áudio atualmente em reprodução
+                currentlyPlaying = soundEl;
             }
         });
     });
